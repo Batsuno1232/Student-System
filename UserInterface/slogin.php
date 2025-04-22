fb82b0468415b4f60b328ba6be9054feb9a5a509
Batsuno1232 <batsu1232@gmail.com>
2025-04-22 11:49:34 +0700

    build login page and button login

--- UserInterface/slogin.php
+++ UserInterface/slogin.php
@@ -4,15 +4,81 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Student Login</title>
     <link href="/Project1/CSS/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/Project1/CSS/mycss.css" rel="stylesheet">
 </head>
 
 <body>
-    <h1>Hello, world!</h1>
-    <script src="/Project1/CSS/Bootstrap/js/bootstrap.bundle.min.js"
-        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
-        crossorigin="anonymous"></script>
+    <div class="mainDiv">
+        <div class="innerDiv border border-danger">
+            <div class="container">
+                <div class="row" style="margin:2%;">
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+                    <div class="col-lg-6 border border-danger">
+                        <label class="text-info" style="font-size: 24px;">ROLL NO</label>
+                    </div>
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+                </div>
+                <div class="row" style="margin:2%;">
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+                    <div class="col-lg-6 border border-danger">
+                        <input class="loginTextBox" type="text">
+                    </div>
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+                </div>
+                <div class="row" style="margin:2%;">
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+                    <div class="col-lg-6 border border-danger">
+                        <label class="text-info">Password</label>
+                    </div>
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+                </div>
+
+                <div class="row" style="margin:2%;">
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+                    <div class="col-lg-6 border border-danger">
+                        <input class="loginTextBox" type="password">
+                    </div>
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+                </div>
+                <div class="row" style="margin:2%;">
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+                    <div class="col-lg-6 border border-danger">
+                        <div style="width: 100%; justify-content: end; display: flex;">
+                            <button class="btn btn-success">Login</button>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-3 border border-danger">
+
+                    </div>
+
+                </div>
+
+
+
+
+            </div>
+            <script src="/Project1/CSS/Bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
