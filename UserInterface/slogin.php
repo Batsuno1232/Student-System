d1e8fe88b1d499beb138c47847bd0b5fc93b7e2b
Batsuno1232 <batsu1232@gmail.com>
2025-04-22 10:38:21 +0700

    add css and js Bootstrap

--- UserInterface/slogin.php
+++ UserInterface/slogin.php
@@ -1,11 +1,18 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="/Project1/CSS/Bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <h1>Welcome</h1>
+    <h1>Hello, world!</h1>
+    <script src="/Project1/CSS/Bootstrap/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
